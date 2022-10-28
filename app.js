b76a0412aea9e81cd9484cf7aba7af2e43418a0b
pliskin <arieljanickcarvalho@gmail.com>
2022-10-28 20:41:38 +0100

    Cleaning up the code.
    Moving reused code into a function and cleaning up extra spaces and unnecessary comments.

--- app.js
+++ app.js
@@ -1,69 +1,79 @@
-const { json } = require("body-parser");
 const express = require("express");
 const app = express();
 const Joi = require("joi");
 app.use(express.json()); // Allow the parsin of json
 
+function validate(input) {
+
+    // input will be the object. Probably the body of the request
+    const schema = {
+        about: Joi.string().min(3).required()
+    };
+
+   // input used to be request.body
+   const result = Joi.validate(input, schema);
+
+   return result;
+}
+
+function findCourse(courses, id) {
+    // id used to be request.params.id
+
+    // array.find calls predicate for every element of the array
+    return courses.find((object) => object.id === parseInt(id));
+
+}
 
 const courses = [
     {id: 1, about: "math"},
     {id: 2, about: "love"}
 ]
 
-// Get requests
 app.get("/", (request, response) => {
-    response.send("Hello there");
+    return response.send("Hello there");
 })
 
 app.get("/courses/:id", (request, response) => {
-    const course = courses.find(function(c) {
-        return c.id === parseInt(request.params.id);
-    })
+    const course = findCourse(courses, request.params.id);
 
     if(!course) {
-        response.status(404).send("No course with that id");
+        return response.status(404).send("No course with that id");
     }
 
     response.send(course);
 });
 
 
-// Post requests
 app.post("/courses", (request, response) => {
-   const schema = {
-    about: Joi.string().min(3).required()
-   };
 
-   const result = Joi.validate(request.body, schema);
+    const result = validate(request.body); // Validate input with joi
 
-   if(result.error){
-    response.status(400).send(result.error.details[0].message);
-   } 
+    if(result.error) {
+        return response.status(400).send(result.error.details[0].message); // Get just the message
+    }
 
-   const course = {
-    id: courses.length + 1,
-    about: request.body.about
-   }; 
+    const course = {
+        id: courses.length + 1,
+        about: request.body.about
+    }; 
 
-   courses.push(course);
+    courses.push(course);
 
    // By convention we return the object we created on post requests
    response.send(course);
 
 });
 
+
 app.put("/courses/:id", (request, response) => {
-    const course = courses.find((c) => c.id === parseInt(request.params.id));
-    if(!course) response.status(404).send("Course does not exist");
+    const course = findCourse(courses, request.params.id);
 
-    const schema = {
-        about: Joi.string().min(3).required()
-    }
+    if(!course) return response.status(404).send("No such course");
 
-    const result = Joi.validate(request.body, schema);
+    const result = validate(request.body); // Validate input with joi 
     
     if(result.error) {
-        return response.status(400).send(result.error.details[0].message);
+        return response.status("400").send(result.error.details[0].message);
     }
 
     // Update the values
@@ -72,16 +82,19 @@ app.put("/courses/:id", (request, response) => {
     return response.send(course);
 });
 
+
 app.delete("/courses/:id", (request, response) => {
-    const course = courses.find((c) => c.id === parseInt(request.params.id));
+    const course = findCourse(courses, request.params.id);
+
     if(!course) response.status(404).send("Course does not exist");
     
     const index = courses.indexOf(course);
     courses.splice(index, 1);
 
-    response.send(course);
-})
+    return response.send(course);
+});
 
 // console.log(process.env.PORT); -- To read environment variable
+
 app.listen(3000);
 console.log("listening on 3000");
